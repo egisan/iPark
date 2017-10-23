1918c55fd1223cac2fab2761176354ccb1e9dc79
Egidio Ricchiuti <egisan3@yahoo.com>
2017-10-23 14:55:06 +0200

    Change to LocalDb and new migrations

--- iPark/Migrations/201710231241304_Init.Designer.cs
+++ iPark/Migrations/201710231241304_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace iPark.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class TimeFields : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(TimeFields));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201710230943240_TimeFields"; }
+            get { return "201710231241304_Init"; }
         }
         
         string IMigrationMetadata.Source
