452453f38d5c105769fdd648323886c107638775
James Allen <james_allen.irish@yahoo.com>
2017-10-25 09:18:56 +0200

    FixesJames

--- iPark/Models/Vehicle.cs
+++ iPark/Models/Vehicle.cs
@@ -11,7 +11,7 @@ namespace iPark.Models
         public int Id { get; set; }
 
         [Required]
-        [StringLength(7, ErrorMessage = "The Last {0} length can't be more {1} signs", MinimumLength =6)]
+        [StringLength(7, ErrorMessage = "{0}: Can't be more than {1} characters", MinimumLength =6)]
         public string RegNo { get; set; }
 
         [Required]
@@ -21,11 +21,11 @@ namespace iPark.Models
         public EnumEntities.Vtypes VehichleType { get; set; }
 
         [Required]
-        [StringLength(30, ErrorMessage = "The Last {0} length can't be more {1} signs", MinimumLength = 3)]
+        [StringLength(30, ErrorMessage = "{0}: Can't be less than {2} or more than {1} characters", MinimumLength = 2)]
         public string Make { get; set; }
 
         [Required]
-        [StringLength(30, ErrorMessage = "The Last {0} length can't be more {1} signs", MinimumLength =3)]
+        [StringLength(30, ErrorMessage = "{0}: Can't be less than {2} or more than {1} characters", MinimumLength =3)]
         public string Model { get; set; }
 
         [Range(2, 12, ErrorMessage = "The number of {0} must be between {1} and {2}")]
